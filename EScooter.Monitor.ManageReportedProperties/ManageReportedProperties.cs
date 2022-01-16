b6206d34c79fac4676df7a034940a84ad9d92c6b
Samuele <samuele.bura98@gmail.com>
2022-01-16 11:15:10 +0100

    Initialize static clients

--- EScooter.Monitor.ManageReportedProperties/ManageReportedProperties.cs
+++ EScooter.Monitor.ManageReportedProperties/ManageReportedProperties.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Text.Json;
-using Azure;
 using Azure.DigitalTwins.Core;
 using Azure.Identity;
-using Microsoft.Azure.Devices.Shared;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -13,19 +9,26 @@ namespace EScooter.Monitor.ManageReportedProperties
 {
     public static class ManageReportedProperties
     {
+        private static readonly DigitalTwinsClient _digitalTwinsClient = InstantiateDtClient();
+
+        private static DigitalTwinsClient InstantiateDtClient()
+        {
+            string digitalTwinUrl = "https://" + Environment.GetEnvironmentVariable("AzureDTHostname");
+            var credential = new DefaultAzureCredential();
+            var digitalTwinsClient = new DigitalTwinsClient(new Uri(digitalTwinUrl), credential);
+            return digitalTwinsClient;
+        }
+
         [Function("manage-properties")]
         public static async void ManageProperties([ServiceBusTrigger("%TopicName%", "%SubscriptionName%", Connection = "ServiceBusConnectionString")] string mySbMsg, FunctionContext context)
         {
             var logger = context.GetLogger("Function");
-            var digitalTwinUrl = "https://" + Environment.GetEnvironmentVariable("AzureDTHostname");
-            var credential = new DefaultAzureCredential();
-            var digitalTwinsClient = new DigitalTwinsClient(new Uri(digitalTwinUrl), credential);
 
             var scooterStatusChanged = JsonConvert.DeserializeObject<ScooterStatusChanged>(mySbMsg);
             var scooterId = scooterStatusChanged.Id;
             var patch = JsonPatchFactory.GetStatusPatch(scooterStatusChanged);
 
-            await digitalTwinsClient.UpdateDigitalTwinAsync(scooterId, patch);
+            await _digitalTwinsClient.UpdateDigitalTwinAsync(scooterId, patch);
             logger.LogInformation($"Updated reported properties of twin: {scooterId}\n");
         }
     }
